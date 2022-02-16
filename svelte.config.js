6d678a3b3dd7ace00a529e309802c45b007c8cfd
rboddy <avidryanb@gmail.com>
2022-02-16 07:24:35 -0500

    Getting on render

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,26 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter()
+// 	}
+// };
+
+// export default config;
 
 /** @type {import('@sveltejs/kit').Config} */
+
+import node from '@sveltejs/adapter-node';
+
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: node(),
+		
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
 	}
 };
 
